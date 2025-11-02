407cc9685c3c4db21c8ee85b9525ab2dd7b6d3a6
Kgoete <Pontsho.Kgoete@standardbank.co.za>
2025-11-02 18:05:38 +0200

    fix helmet-async

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {HelmetProvider} from 'react-helmet-async'
 import { CallLogProvider } from './Pages/ContextPage/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
       <CallLogProvider>
-        <Helmet>
+        <HelmetProvider>
           <App />
-        </Helmet>
+        </HelmetProvider>
       </CallLogProvider>
     </StrictMode>
 )
