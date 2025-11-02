b78307a55377dacd3daac85de568724146ea4b01
Kgoete <Pontsho.Kgoete@standardbank.co.za>
2025-11-02 17:15:21 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {Helmet} from 'react-helmet-async'
-import { BrowserRouter } from 'react-router-dom'
 import { CallLogProvider } from './Pages/ContextPage/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
