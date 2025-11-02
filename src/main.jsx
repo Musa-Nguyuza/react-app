441ae063fb05c3f605e8d4f41b0e244271107871
Kgoete <Pontsho.Kgoete@standardbank.co.za>
2025-11-02 17:06:14 +0200

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {HelmetProvider} from 'react-helmet-async'
+import {Helmet} from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
 import { CallLogProvider } from './Pages/ContextPage/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
       <CallLogProvider>
-        <HelmetProvider>
+        <Helmet>
           <App />
-        </HelmetProvider>
+        </Helmet>
       </CallLogProvider>
     </StrictMode>
 )
