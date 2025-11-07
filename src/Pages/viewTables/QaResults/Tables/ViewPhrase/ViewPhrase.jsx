b91938cce4e7b255ba00e3de213e360cb8e6ce35
Kgoete <Pontsho.Kgoete@standardbank.co.za>
2025-11-07 09:45:07 +0200

    api added

--- src/Pages/viewTables/QaResults/Tables/ViewPhrase/ViewPhrase.jsx
+++ src/Pages/viewTables/QaResults/Tables/ViewPhrase/ViewPhrase.jsx
@@ -29,7 +29,7 @@ const ViewPhrase = () => {
 
   useEffect(() => {
     // Fetch the full object to extract Contact ID and Policy Number
-    fetch(`http://localhost:3001/api/data/${id}`)
+    fetch(`https://riskapp-backend.onrender.com/api/data/${id}`)
       .then((res) => res.json())
       .then((data) => {
         setMetaData({
@@ -62,7 +62,7 @@ const ViewPhrase = () => {
   //get the id/tablename and index in dispute.json
 const handleViewFinding = async (phrase, policyNumber) => {
   try {
-    const res = await fetch('http://localhost:3001/api/find-phrase', {
+    const res = await fetch('https://riskapp-backend.onrender.com/api/find-phrase', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ phrase, policyNumber })
